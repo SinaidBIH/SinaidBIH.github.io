881c5bf179b4750a6467e87837e28f3faa6fb7d3
Sinaid Selimovic <94962039+SinaidBIH@users.noreply.github.com>
2023-11-08 19:36:21 +0100

    goeie werkende hobby's gallery en een projects.php

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="media/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:600,900" rel="stylesheet">
   <title>Sinaid Selimovic</title>
 </head>
-
 <body>
   <header class="header">
     <a href="#default" class="logo">Sinaid Selimovic</a>
     <nav class="header-right">
-      <a class="active" href="#home">Home</a>
-      <a href="#Projecten">Projecten</a>
-      <a href="#OverMij">Over Mij</a>
+      <a class="active" href="index.php">Home</a>
+      <a href="projects.php">Projecten</a>
     </nav>
   </header>
 
@@ -29,69 +27,73 @@
     </div>
   </section>
 
-  <section class="info">
-    <div class="centered-info">
-      <h1>Over mij</h1>
-      <br>
-      <h2>Wie ben ik?</h2>
-      <p>Mijn naam is Sinaid Selimovic. Ik ben 18 jaar oud. Mijn hobbies zijn Auto's, Jagen met mijn vader, Gamen, Scooters, Vissen
-        met mijn vader en F1. Ik zit op
-        de opleiding Software Developer niveau 4. Het is een uitdagende maar goede opleiding waar je veel kunt leren.
-      </p>
+  <div class="centered-info">
+    <h1>Over mij</h1>
+    <br>
+    <h2>Wie ben ik?</h2>
+    <p>Mijn naam is Sinaid Selimovic. Ik ben 18 jaar oud</p>
+       <p>Mijn hobby's zijn: Houden van totaal onpopulaire auto's, Jagen met mijn vader, Gamen, Scooters, F1 en
+         Vissen met mijn vader. Ik zit op de opleiding Software Developer niveau 4. 
+         Het is een uitdagende maar goede opleiding waar je veel kunt leren.</p>
+  </div>
+
+  <div class="gallery-image">
+    <div class="img-box">
+      <img src="media/kia.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Kia k5 gt</p>
+          <p class="opacity-low">Prachtig exemplaar</p>
+        </div>
+      </div> 
     </div>
-    <ul class="centered-image-list">
-      <li>
-        <a href="#">
-          <figure>
-            <img src="media/kia.jpg" alt="auto">
-            <figcaption>Mijn droom auto</figcaption>
-          </figure>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <figure>
-            <img src="media/jagen.jpg" alt="jagen">
-            <figcaption>Jagen met mijn vader</figcaption>
-          </figure>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <figure>
-            <img src="media/desktop.jpg" alt="desktop">
-            <figcaption>Gaming desktop</figcaption>
-          </figure>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <figure>
-            <img src="media/zip.jpg" alt="scooter">
-            <figcaption>Mijn scooter</figcaption>
-          </figure>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <figure>
-            <img src="media/aston.jpg" alt="f1 auto">
-            <figcaption>Formule 1</figcaption>
-          </figure>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <figure>
-            <img src="media/snoek.jpg" alt="snoek">
-            <figcaption>Mijn vader</figcaption>
-          </figure>
-        </a>
-      </li>
-    </ul>
-  </section>
+    <div class="img-box">
+      <img src="media/jagen.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Jagen</p>
+          <p class="opacity-low">Goede workout voor in de winter</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="media/desktop.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Gamen</p>
+          <p class="opacity-low">Helpt met relaxen (soms)</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="media/zip.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Piaggio zip</p>
+          <p class="opacity-low">Mijn mooie scooter</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="media/aston.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Aston martin</p>
+          <p class="opacity-low">Bestuurd door Lance Stroll</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="media/snoek.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Snoekbaars</p>
+          <p class="opacity-low">Gevangen door mijn vader en een helaas overleden kennis</p>
+        </div>
+      </div> 
+    </div>
+  </div>
 
   <?php include 'footer.php'; ?>
 </body>
-
 </html>
---
+++ projects.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="media/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Mijn Projecten</title>
+    <style>
+        .gallery {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .gallery img {
+            width: 300px;
+            height: 200px;
+            margin: 10px;
+            border: 1px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+
+<header class="header">
+    <a href="#default" class="logo">Sinaid Selimovic</a>
+    <nav class="header-right">
+      <a class="active" href="index.php">Home</a>
+      <a href="projects.php">Projecten</a>
+    </nav>
+  </header>
+
+    <h1>Mijn Projecten</h1>
+    <p>Hier zijn enkele van mijn projecten:</p>
+
+    <div class="gallery">
+        <img src="blank-image1.jpg" alt="Afbeelding 1">
+        <img src="blank-image2.jpg" alt="Afbeelding 2">
+        <img src="blank-image3.jpg" alt="Afbeelding 3">
+    </div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend dapibus metus, et dapibus justo bibendum nec. Nullam at justo vel justo hendrerit facilisis. Integer auctor arcu a justo ullamcorper, et imperdiet massa convallis. Vestibulum bibendum est nec nisl efficitur bibendum. Sed sed justo nec lectus tristique interdum.</p>
+
+    <p>Phasellus eget odio non sapien varius ultrices. Vivamus mattis felis et lacus egestas, in facilisis nisl tristique.
+         Suspendisse quis dui et neque bibendum viverra in eget est. Sed feugiat felis id dui semper, sit amet feugiat ligula elementum.</p>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
